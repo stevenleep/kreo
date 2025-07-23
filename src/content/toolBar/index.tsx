950de249017104b585c74b9dacb659fb8de16b70
zcj <1248198090@qq.com>
2025-07-23 12:06:17 +0800

    feat:dev

--- src/content/toolBar/index.tsx
+++ src/content/toolBar/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import styles from './index.module.less';
 
 const ToolBar = () => {
+    const handlerDraw = () => {
+        alert(1);
+    };
+
     return (
-        <div>
-            12121
+        <div className={styles.tool_bar}>
+            <div onClick={handlerDraw}>rect</div>
         </div>
     )
 }
